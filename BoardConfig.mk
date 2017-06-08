e06a8456b5b8ea03e73d92a18a3d6d3d4bd834f7
Rygebin <kaankulahli@gmail.com>
2017-06-08 11:26:52 +0000

    shamrock: Add more changes
    * Complete upgrade for N.
    * Remove full of TW_CRYPTO.
    * Add new prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,19 +36,11 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Kernel
-BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-ENABLE_CPUSETS := true
-TARGET_KERNEL_SOURCE := kernel/google/shamrock
-TARGET_KERNEL_CONFIG := lineageos_shamrock_defconfig
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+BOARD_KERNEL_CMDLINE := ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --kernel_offset 0x00008000 --second_offset 0x00f00000 --dt $(DEVICE_PATH)/prebuilt/dt.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 
 # Partitions
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -66,8 +58,16 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 28538268672
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/twrp.fstab
 
-# TWRP - CRYPTO
-TARGET_HW_DISK_ENCRYPTION := true
+# TWRP
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_SUPERSU := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_INCLUDE_CRYPTO := true
+TW_THEME := portrait_hdpi
 
 # TWRP - Remove mouse pointer
 TW_INPUT_BLACKLIST := "hbtp_vm"
